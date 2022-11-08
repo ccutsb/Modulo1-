d98ad2eccd5e8cfdaf492e7fad0efb21a48c96f3
allmi6ht <cristiancortes9297@gmail.com>
2022-11-08 11:14:00 -0300

    Ejercicio 2 listo

--- Ejercicio2/main2.js
+++ Ejercicio2/main2.js
@@ -1,6 +1,3 @@
-// VARIABLES 
-// FORMAS DE DECLARAR VARIABLES:
-// VAR  -  LET  -  CONST
 /* Crea una variable llamada nombre y asígnale tu nombre como valor.
  Crea una variable llamada edad y asígnale tu edad como valor.
  Crea una variable llamada mayorDeEdad y asígnale un valor booleano que indique si eres mayor de edad.
@@ -10,14 +7,16 @@
  Crea una variable llamada mejorLenguaje y asígnale el primer elemento del array lenguajesFavoritos.
  Crea una variable llamada peorLenguaje y asígnale el último elemento del array lenguajesFavoritos. 
 */
+// Crea una variable llamada nombre y asígnale tu nombre como valor.
+const nombre = 'Cristian';
 
-const nombre = 'Cristian' ;
+// Crea una variable llamada edad y asígnale tu edad como valor.
+let edad = 26;
 
-let edad = 26 ;
+// Crea una variable llamada mayorDeEdad y asígnale un valor booleano que indique si eres mayor de edad.
+var mayorDeEdad = true;
 
-var mayorDeEdad = true > 18
-
-
+// Crea una variable llamada direccion y asígnale un objeto con tu dirección.
 const direccion = {
     calle: 'La plaza',
     numero: 5698,
@@ -25,16 +24,18 @@ const direccion = {
     region: 'Santiago',
     Pais: 'Chile'
 };
-
+// Crea una variable llamada coloresFavoritos y asígnale un array con tus colores favoritos.
 const coloresFavoritos = ['verde', 'morado', 'rojo', 'rosado', 'negro'] ;
 
+// Crea una variable llamada lenguajesFavoritos y asígnale un array con tus lenguajes de programación favoritos.
 const lenguajesFavoritos = ['JavaScript', 'Python', 'Kotlin'] ;
 
-const mejorLenguaje = lenguajesFavoritos[0] 
-    console.log (mejorLenguaje);
+// Crea una variable llamada mejorLenguaje y asígnale el primer elemento del array lenguajesFavoritos.
+const mejorLenguaje = lenguajesFavoritos[0];
 
-const peorLenguaje = lenguajesFavoritos[2]
-    console.log (peorLenguaje) ;
+// Crea una variable llamada peorLenguaje y asígnale el último elemento del array lenguajesFavoritos. 
+const peorLenguaje = lenguajesFavoritos[lenguajesFavoritos.length - 1]
+      console.log(peorLenguaje)
 
 
 
