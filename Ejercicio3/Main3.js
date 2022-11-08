23dc455c0d2baf87b51dd45b3b51798adc36ee60
allmi6ht <cristiancortes9297@gmail.com>
2022-11-08 11:41:32 -0300

    Ejercicio 3 terminado

---
+++ Ejercicio3/Main3.js
@@ -0,0 +1,26 @@
+//Ejercicio 3
+//Crea dos variables, una llamada numero1 y otra llamada numero2.
+//Suma las dos variables y muestra el resultado por consola.
+//Crea otras variables de tipo number y realiza las operaciones de resta, multiplicación y división. Mostrando el resultado por consola.
+
+//Crea dos variables, una llamada numero1 y otra llamada numero2.
+const numero1 = 35;
+const numero2 = 21;
+const suma = numero1 + numero2;
+//Suma las dos variables y muestra el resultado por consola.
+console.log(suma);
+//Crea otras variables de tipo number y realiza las operaciones de resta, multiplicación y división. Mostrando el resultado por consola.
+const numero3 = 46;
+const numero4 = 22;
+const resta = numero3 - numero4;
+console.log(resta);
+
+const numero5 = 84;
+const numero6 = 4;
+const multiplicacion = numero5 * numero6; 
+console.log(multiplicacion);
+
+const numero7 = 5642;
+const numero8 = 560;
+const division = numero7 / numero8;
+console.log(division);
