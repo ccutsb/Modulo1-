6f57b04873a72c6564101776741ec58a34216757
allmi6ht <cristiancortes9297@gmail.com>
2022-11-08 11:56:17 -0300

    Ejercicio 4 terminado

---
+++ Ejercicio4/Main4.js
@@ -0,0 +1,11 @@
+/* Ejercicio 4
+ Crea una variable llamada saludo y asígnale un string que contenga hola.
+ Comprueba con un if si el string saludo es igual a hola. Y saca por consola un mensaje que diga "Ha saludado".*/
+ 
+ const saludo = 'Hola';
+
+ if (saludo === 'Hola'){
+    console.log('Ha saludado')
+ }else{
+    console.log('No te saluda')
+ }
